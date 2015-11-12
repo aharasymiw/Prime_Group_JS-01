dbba85a436db9495d9435292516d978f0da294db
Andrew Harasymiw <aharasymiw@gmail.com>
2015-11-12 13:12:12 -0600

    create file structure and start coding js file

---
+++ script.js
@@ -0,0 +1,8 @@
+//[name, empNumber, annualSalary, reviewRating]
+
+var Employee = function (name, employeeNumber, annualSalary, reviewRating) {
+	this.name = name;
+	this.employeeNumber = employeeNumber;
+	this.annualSalary = annualSalary;
+	this.reviewRating = reviewRating;
+}
